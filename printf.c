f6923473cda7e8f911f4ff09185059aa3e0d4789
SKwambua <samuelkikuvi33@gmail.com>
2022-04-22 22:36:27 +0000

    fix error

--- printf.c
+++ printf.c
@@ -39,7 +39,7 @@ int (*get_op(const char b))(va_list)
 int _printf(const char *format, ...)
 {
 	va_list list;
-	int len = 0, i = 0;
+	int lens= 0, i = 0;
 	int (*func)();
 
 	if (!format || (format[0] == '%' && format[1] == '\0'))
@@ -57,12 +57,12 @@ int _printf(const char *format, ...)
 			if (func == NULL)
 			{
 				_putchar(format[i]);
-				len++;
+				lens++;
 				i++;
 			}
 			else
 			{
-				len += func(list);
+				lens += func(list);
 				i += 2;
 				continue;
 			}
@@ -70,10 +70,10 @@ int _printf(const char *format, ...)
 		else
 		{
 			_putchar(format[i]);
-			len++;
+			lens++;
 			i++;
 		}
 	}
 	va_end(list);
-	return (len);
+	return (lens);
 }
